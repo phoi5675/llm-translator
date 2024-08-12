94fb9928cdaf562ebc9b9d2ea37e384777b4f6f4
phoi5675 <phoi5675@gmail.com>
2024-08-12 21:07:54 +0900

    Fix main.py
    - Fix params passing to `translate()`

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import sys
 from core.translate import get_default_model, translate
 
 parser = argparse.ArgumentParser(
-    description="prints translated string or string array into desired language using madlad400-3b-mt")
+    description="Prints translated string or string array into desired language using madlad400-3b-mt")
 parser.add_argument('--to', action='store_const', default='ko',
                     help='ISO code of destination language. defaults to ko(Korean). See https://huggingface.co/languages for language codes')
 parser.add_argument('input_text', nargs='?',
@@ -17,14 +17,15 @@ parser.add_argument('input_text', nargs='?',
 
 args = parser.parse_args()
 
-model, tokenizer = get_default_model()
 
 if __name__ == '__main__':
+    model, tokenizer = get_default_model()
+
     lang_to = args.to
     # If input_text is sigle-line string
     if args.input_text:
         text = args.input_text
-        print(translate(text, lang_to))
+        print(translate(model, tokenizer, text, lang_to))
     # If input_text is piped, multi-line string
     else:
         text_list = sys.stdin.read().splitlines()
